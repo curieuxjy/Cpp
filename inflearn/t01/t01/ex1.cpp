d82556cbfc6489ec207042686db507e427cbaae7
Jungyeon Lee <skyfiower9721@gmail.com>
2020-09-14 22:16:13 +0900

    no message

---
+++ inflearn/t01/t01/ex1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+// 이 프로그램이 stdio.h을 포함
+// printf가 무엇인지 알려주는 역할
+
+int main() {
+	printf("Hello, World!\n");
+	printf("My name is Jungyeon\n");
+}
+// main() 함수
+// main을 우선적으로 실행
+// printf도 함수, 문자열 출력
+// \n은 enter
+// ;마침표 잊지말것!
---
+++ inflearn/t03/t03/ex1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("%d + %d = %d\n", 2, 3, 5);
+
+	printf("%f\n", 3.14);
+	//printf()
+
+}
